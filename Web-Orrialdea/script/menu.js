6ad435b61f0b0aaf072915367b3a1ff762ecf57d
Ander <115074583+EeeeSIGracias@users.noreply.github.com>
2023-10-11 12:26:48 +0200

    a

--- Web-Orrialdea/script/menu.js
+++ Web-Orrialdea/script/menu.js
@@ -1,4 +1,10 @@
 function menuopenclose () {
-    document.getElementById("menu")
-
+    var menu = document.getElementById("menu")
+    if (menu.hidden) {
+        menu.hidden = false
+        document.getElementById("negro").hidden = false
+    } else {
+        menu.hidden = true
+        document.getElementById("negro").hidden = true
+    }  
 }
