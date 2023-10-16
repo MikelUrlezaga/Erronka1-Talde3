723a4d2a7b07773ecfd43c6de053d77cc9bf3cb2
Ander <115074583+EeeeSIGracias@users.noreply.github.com>
2023-10-16 08:54:34 +0200

    Menuseparado :D

--- Web-Orrialdea/script/menu.js
+++ Web-Orrialdea/script/menu.js
@@ -1,3 +1,10 @@
+// Insertar menu
+fetch('../HTML/Menu.html')
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('menuMugikorra').innerHTML = data;
+            });
+
 function menuopenclose () {
     var menu = document.getElementById("menu")
     if (menu.hidden) {
